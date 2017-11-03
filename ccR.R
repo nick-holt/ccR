cc6ba53eeed2939be77171c7ceda8dad5b5b82db
Nick Holt <naholt01@gmail.com>
2017-11-03 16:14:29 -0400

    Simplified code from initial build

--- ccR.R
+++ ccR.R
@@ -32,25 +32,12 @@ library(stringr)
 library(lubridate)
 library(purrr)
 
-# converter function for JSON to df from API
-
+#----------------------------------------------
+# Transform Campaign JSON into data frame
+#----------------------------------------------
 getCampaignData <- function(campaignJSON) {
-        idlist <- NULL
-        namelist <- NULL
-        urllist <- NULL
-        statuslist <- NULL
-        datelist <- NULL
         JSONList <- jsonlite::fromJSON(campaignJSON)
         results <- JSONList$results
-        #for (i in 1:length(results)) {
-        #        namelist <- c(namelist, results[i][[1]]$name )
-        #        urllist <- c(urllist, paste("https://api.constantcontact.com/v2/emailmarketing/campaigns/", results[i][[1]]$id, sep="", collapse=NULL))
-        #        statuslist <- c(statuslist, results[i][[1]]$status)
-        #        datelist <- c(datelist, results[i][[1]]$modified_date)
-        #        idlist <- c(idlist, results[i][[1]]$id)
-        #}
-        #campaignDF = data.frame(id = idlist, name = namelist, url = urllist, status = statuslist, last_modified_date=datelist,stringsAsFactors=FALSE) %>%
-        #        mutate(last_modified_date = ymd(str_extract(last_modified_date, "\\d+-\\d+-\\d+")))
         return(results)
 }
 
@@ -77,35 +64,9 @@ extractAllCampaigns <- function(){
 #-------------------------------------------------------------
 # Function to extract information from a page of contact info
 #-------------------------------------------------------------
-
-contactsJSON = getURL("https://api.constantcontact.com/v2/emailmarketing/campaigns/1120949536316/tracking/sends?next=bGltaXQ9NTAwJm5leHQ9MTExMzY3NzA1NDU1MSVhZTY2MDk2NS1mMDUzLTQwZmEtOTg2OS04NjI1Y2VjZjdlN2IlQUFnQUFBRk5JOGhRd0FBQUVGTlh4Q0IrU1JIamlhN1VybEthaDRZQUFCQlVQOW93ZmtrUjQ0cEExSzVTbW9lR0FBQUFBQT09&access_token=543671a7-e5cb-4c37-8029-13101d5bfbf1&api_key=qy2gc5nd3pw5g6rj6tmc5zwt")
-
-# function to get contacts for a given campagin id
 getCampaignContacts <- function(contactsJSON) {
-        
-        # initialize lists
-        #idlist <- NULL
-        #emaillist <- NULL
-        #activitylist <- NULL
-        #campaignlist <- NULL
-        #datelist <- NULL
-        
-        # convert JSON to list
         JSONList <- jsonlite::fromJSON(contactsJSON)
         results <- JSONList$results
-        
-        # loop over elements and extract information into lists
-        #for (i in 1:length(results)) {
-        #        idlist <- c(idlist, results[i][[1]]$contact_id)
-        #        emaillist <- c(emaillist, results[i][[1]]$email_address)
-        #        activitylist <- c(activitylist, results[i][[1]]$activity_type )
-        #        campaignlist <- c(campaignlist, results[i][[1]]$campaign_id)
-        #        datelist <- c(datelist, results[i][[1]]$send_date)
-        #}
-        
-        # combine list results in to data frame
-        #contactsDF <- data.frame(contact_id = idlist, email = emaillist, activity = activitylist, campaign_id = campaignlist, send_date=datelist, stringsAsFactors=FALSE) %>%
-        #        mutate(send_date = ymd(str_extract(send_date, "\\d+-\\d+-\\d+")))
         return(results)
 }
 
@@ -133,8 +94,6 @@ paginator <- function(start_url){
 #-------------------------------------------------------------------------------
 # Function to loop over pagination within a campaign and extract all contacts
 #-------------------------------------------------------------------------------
-campaign_id <- target_ids[14]
-
 extractCampaignContacts <- function(campaign_id){
         
         # set contacts.dataframe to NULL
@@ -158,8 +117,3 @@ extractCampaignContacts <- function(campaign_id){
         }
         return(contacts.dataframe)
 }
-
-
-
-
-
